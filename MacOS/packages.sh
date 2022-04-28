d651db1ce27d4c80f4017b1ba688a23cadaeb855
Abhijeet Nazar <abhijeet.nazar@gmail.com>
2022-04-28 22:50:06 +0200

    Added homebrew mac packages

---
+++ MacOS/packages.sh
@@ -0,0 +1,46 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Multimedia
+brew install --cask 5kplayer
+brew install --cask vlc
+brew install --cask obs
+brew install --cask xnviewmp
+brew install --cask spotify
+brew install --cask audacity
+brew install --cask handbrake
+brew install --cask gimp
+brew install --cask kdenlive
+# Google
+brew install --cask google-drive
+brew install --cask google-chrome
+# MS
+brew install --cask microsoft-office
+brew install --cask microsoft-edge
+# Office
+brew install --cask zoom
+brew install --cask adobe-acrobat-reader
+brew install --cask calibre
+brew install --cask coteditor
+# System
+brew install --cask shottr
+brew install --cask kindle
+brew install --cask qbittorrent
+brew install --cask transnomino
+brew install --cask iriunwebcam
+brew install --cask vnc-server
+brew install --cask vnc-viewer
+brew install --cask microsoft-remote-desktop
+brew install --cask anydesk
+brew install --cask alt-tab
+brew install --cask alfred
+brew install openvpn
+# Development
+brew install --cask visual-studio-code
+brew install --cask anaconda
+brew install --cask postman
+brew install --cask dbeaver-community
+brew install --cask pycharm-ce
+brew install git
+brew install --cask sourcetree
+brew install --cask oracle-jdk
+brew install --cask whatsapp
---
+++ MacOS/packages_loop.sh
@@ -0,0 +1,58 @@
+echo "Installing homebrew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "Installing multimedia tools..."
+multimedia=('5kplayer'	 'vlc' 'obs' 'xnviewmp' 'spotify' 'audacity' 'handbrake' 'gimp'	 'kdenlive')
+for i in "${multimedia[@]}"
+do
+	echo "brew install --cask $i"
+done
+
+echo "Installing google tools..."
+google=('google-drive'	'google-chrome')
+for i in "${google[@]}"
+do
+	echo "brew install --cask $i"
+done
+
+echo "Installing ms tools..."
+ms=('microsoft-office' 'microsoft-edge')
+for i in "${ms[@]}"
+do
+	echo "brew install --cask $i"
+done
+
+echo "Installing office tools..."
+office=('zoom' 'adobe-acrobat-reader' 'calibre' 'coteditor')
+for i in "${office[@]}"
+do
+	echo "brew install --cask $i"
+done
+
+echo "Installing system tools..."
+system=('shottr' 'kindle' 'qbittorrent'	 'transnomino' 'iriunwebcam' 'vnc-server'	' vnc-viewer' 'microsoft-remote-desktop' 'anydesk' 'ksnip' 'alfred' 'alt-tab')
+for i in "${system[@]}"
+do
+	echo "brew install --cask $i"
+done
+
+echo "Installing development tools..."
+development=('visual-studio-code' 'anaconda' 'postman' 'dbeaver-community'	'pycharm-ce')
+for i in "${development[@]}"
+do
+	echo "brew install --cask $i"
+done
+
+echo "Installing other tools..."
+others=('sourcetree' 'oracle-jdk' 'whatsapp')
+for i in "${others[@]}"
+do
+	echo "brew install --cask $i"
+done
+
+echo "Installing non cask tools..."
+noncask=('git' 'openvpn')
+for i in "${noncask[@]}"
+do
+	echo "brew install $i"
+done

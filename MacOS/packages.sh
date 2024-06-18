089f9d89f94fe05322c1adc2451a40bf226eb780
Abhijeet Nazar <abhijeet.nazar@gmail.com>
2024-06-19 00:48:21 +0200

    mac v0.2

--- MacOS/packages.sh
+++ MacOS/packages.sh
@@ -8,16 +8,26 @@ brew install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 source ~/.zshrc
 
+# New
+brew install --cask losslesscut
+brew install --cask tiles
+brew install --cask free-download-manager
+brew install --cask github
+brew install --cask warp
+# https://www.neatdownloadmanager.com/index.php/en/
+
 # Multimedia 
 brew install --cask 5kplayer # Video player
+brew install --cask elmedia-player # Video player
 brew install --cask vlc # Video player
 brew install --cask obs # Screen recorder
 brew install --cask xnviewmp # Image viewer
 brew install --cask spotify # Music player
 brew install --cask audacity # Audio editor
 brew install --cask handbrake # Video converter
-brew install --cask gimp # Image editor
 brew install --cask kdenlive # Video editor
+brew install --cask avidemux # Video editor
+brew install --cask affinity-photo # Photo editor
 # Google
 brew install --cask google-drive
 brew install --cask google-chrome
@@ -29,36 +39,30 @@ brew install --cask zoom
 brew install --cask adobe-acrobat-reader
 brew install --cask calibre
 brew install --cask coteditor
-brew install --cask obsidian
+
 # System
 brew install --cask shottr # screenshot tool
 brew install --cask kindle # Book reader
 brew install --cask qbittorrent # Bit torrent client
 brew install --cask transnomino # Bulk file renamer for Mac
 brew install --cask iriunwebcam # Convert android phone to webcam
-brew install --cask vnc-server # Remote desktop server
-brew install --cask vnc-viewer # Remote desktop client
 brew install --cask microsoft-remote-desktop # Windows remote desktop client
 brew install blackhole-16ch
-# brew install --cask anydesk # Remote desktop managment
+
 brew install --cask alt-tab # Windows like functionaltiy on mac
 brew install --cask alfred # Spotlight replacement and works better than spotlight
 brew install --cask rectangle # Window Manager for mac
-brew install --cask brackets
 brew install --cask pensela # Open source tool for screen annotation
-brew install --cask hiddenbar # Hide dock and menu bar
 brew install --cask openvpn-connect # VPN client
+
 # Development
 brew install --cask visual-studio-code
+brew install --cask phoenix-code
 brew install --cask anaconda
 brew install --cask postman
 brew install --cask dbeaver-community
-brew install --cask pycharm-ce
-brew install --cask sourcetree
-# brew install --cask oracle-jdk
+brew install --cask oracle-jdk
 brew cask install java
-
-brew install scala
 brew install apache-spark
 brew install git
 brew install docker
@@ -69,3 +73,14 @@ brew install --cask whatsapp
 # Others
 brew install --cask onyx # cleanmymac
 brew install --cask appcleaner # clean up apps
+
+
+# Legacy
+# brew install --cask gimp # Image editor
+# brew install --cask obsidian
+# brew install --cask anydesk # Remote desktop managment
+# brew install --cask hiddenbar # Hide dock and menu bar
+# brew install scala
+# brew install --cask pycharm-ce
+# brew install --cask sourcetree
+# brew install --cask brackets
